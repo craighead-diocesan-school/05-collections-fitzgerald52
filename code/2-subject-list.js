5d15c5e0f73716d0ccd59ecd8ea8fd1a4f014198
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-19 11:30:47 +1200

    the program allows users to add more subjects untill they say stop and choose which subjects to delete

--- code/2-subject-list.js
+++ code/2-subject-list.js
@@ -12,15 +12,27 @@ function showSubjects() {
 }
 
 function addSubject() {
-  let newSubject = 'Maths'
+  let answer = ''
+  while(answer != 'stop'){
+  let newSubject = prompt('choose a new subject to add')
 
   // add a new item to the end of the subjects array
   subjects.push(newSubject)
+  answer = prompt('do you want to add another subject, if not say stop')
+  }
 }
 
 function removeSubject() {
-  let index = 0
+
+  let index = prompt('choose a number to delete a subject')
+  index = Number(index)
+  
+  let arrayLength = subjects.length
+  if (index < arrayLength){
 
   // remove 1 item at the index position of the subjects array
   subjects.splice(index, 1)
+}else{
+  alert('index value invalid')
+}
 }
