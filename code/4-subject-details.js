95f1e9f2a66e877ef80ccf677bbcaf609cd2e92a
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-19 11:48:10 +1200

    the program lets the user change the name of the teacher and know the room and subject

--- code/4-subject-details.js
+++ code/4-subject-details.js
@@ -3,17 +3,25 @@
 // #####################################
 
 let subject = {
-  name: 'DigiTech',
-  room: 'Room 9',
+  name: 'DigiTech ',
+  room: 'Room 9 ',
   students: 150,
+  teacher: 'random middle-aged man'
 }
 
 function showSubject() {
-  alert(subject.name)
+  alert(subject.name + subject.room)
 }
 
 function changeSubject() {
   let newName = 'Typing Skills'
 
   subject.name = newName
+}
+
+function changeTeacher () {
+  let newTeacher = prompt('what is your teachers new name?')
+
+  subject.teacher = newTeacher
+  alert( 'your teachers name is ' + subject.teacher)
 }
