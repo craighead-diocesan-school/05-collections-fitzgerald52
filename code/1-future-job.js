9daec19fb36a446d4118e61209e6199432c53d93
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-19 09:34:55 +1200

    i made the program generate a random number that also randomly generates a occupation

--- code/1-future-job.js
+++ code/1-future-job.js
@@ -3,14 +3,16 @@
 // ##################################
 
 let jobs = [
-  'astronaut',
-  'firefighter',
-  'police officer',
-  'space doctor',
+  'Astronaut ',
+  'Firefighter ',
+  'Police officer ',
+  'Space doctor ',
+  'Microbiologists  ',
+  'Teacher ',
 ]
 
 function getJob() {
-  let index = 0
-
-  alert('Your future job will be' + jobs[0])
+  let index = Math.floor(Math.random () * 5)
+  index = Number(index) 
+  alert('Your future job will be ' + jobs[index])
 }
