945df89f9abd80f49e79326cc8f7adb4e2cb0877
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-19 11:40:10 +1200

    the program now lets the user input a number to find and can output alerts if the search was a success or not

--- code/3-number-search.js
+++ code/3-number-search.js
@@ -7,11 +7,15 @@ let numberArray = [
   100,
   1000,
   10000,
-  1234567890
+  1234567890,
+  35,
+  26,
+  902
 ]
 
 function searchNumbers() {
-  let searchTarget = 1
+  let searchTarget = prompt('what number do you want to search for ?')
+  searchTarget = Number(searchTarget)
   let searchResult = false
 
   for (let currentNumber of numberArray) {
@@ -19,6 +23,10 @@ function searchNumbers() {
       searchResult = true
     }
   }
+if (searchResult == true){
+alert(searchTarget + ' found: ' + searchResult + ' congrats ' + searchTarget + ' was found !!!')
+} else {(searchResult == false )
+  alert(searchTarget + ' found: ' + searchResult + ' sorry that number can not be found ')
+}
 
-  alert(searchTarget + ' found: ' + searchResult)
 }
